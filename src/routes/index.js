049a9f7a40a2935c8fdc8db6b2a96b6bbe20700d
GMCamargo <bielmarcom@gmail.com>
2020-08-29 16:57:38 -0300

    Correções no commit anterior

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import {SafeAreaView, Text} from 'react-native'
 import {NavigationContainer} from '@react-navigation/native'
-import Stack from './Stack'
+import Navegation from './Navegation'
 
 export default props => (
     <SafeAreaView style={{flex:1, justifyContent: 'center'}}>
         <NavigationContainer>
-            <Stack/>
+            <Navegation/>
         </NavigationContainer>
     </SafeAreaView>
 )
